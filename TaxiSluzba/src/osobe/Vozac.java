e93f8f75e6d308b168d40b9bd123224247a871bf
Miloš Sikimić <sikimic10@gmail.com>
2021-04-19 17:36:38 +0200

    .

--- TaxiSluzba/src/osobe/Vozac.java
+++ TaxiSluzba/src/osobe/Vozac.java
@@ -1,5 +1,8 @@
 package osobe;
 
 public class Vozac extends Korisnik {
+	
+	private double plata;
+	private String brojClanskeKarte;
 
 }
--- TaxiSluzba/src/taxiSluzba/TaksiSluzba.java
+++ TaxiSluzba/src/taxiSluzba/TaksiSluzba.java
@@ -1,5 +1,87 @@
 package taxiSluzba;
 
 public class TaksiSluzba {
+	
+	private int PIB;
+	private String naziv;
+	private String adresa;
+	private double cenaStartaVoznje;
+	private double cenaPoKilometru;
+	
+	
+	private TaksiSluzba() {
+		
+		this.PIB = 0;
+		this.naziv = "";
+		this.adresa = "";
+		this.cenaStartaVoznje = 0;
+		this.cenaPoKilometru = 0;
+		
+	}
+
+
+	public TaksiSluzba(int pIB, String naziv, String adresa, double cenaStartaVoznje, double cenaPoKilometru) {
+		super();
+		PIB = pIB;
+		this.naziv = naziv;
+		this.adresa = adresa;
+		this.cenaStartaVoznje = cenaStartaVoznje;
+		this.cenaPoKilometru = cenaPoKilometru;
+	}
+
+
+	public int getPIB() {
+		return PIB;
+	}
+
+
+	public void setPIB(int pIB) {
+		PIB = pIB;
+	}
+
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+
+	public double getCenaStartaVoznje() {
+		return cenaStartaVoznje;
+	}
+
+
+	public void setCenaStartaVoznje(double cenaStartaVoznje) {
+		this.cenaStartaVoznje = cenaStartaVoznje;
+	}
+
+
+	public double getCenaPoKilometru() {
+		return cenaPoKilometru;
+	}
+
+
+	public void setCenaPoKilometru(double cenaPoKilometru) {
+		this.cenaPoKilometru = cenaPoKilometru;
+	}
+	
+	
+	
 
 }
+
+
