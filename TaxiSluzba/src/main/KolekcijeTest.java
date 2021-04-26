7e8012e902c0118dc3fdb503967f21b87068702e
Miloš Sikimić <sikimic10@gmail.com>
2021-04-26 11:22:05 +0200

    zavrseno sve za automobile i vozace

--- TaxiSluzba/src/main/KolekcijeTest.java
+++ TaxiSluzba/src/main/KolekcijeTest.java
@@ -2,18 +2,33 @@ package main;
 
 import java.util.ArrayList;
 
+import enumeracija.ModelAutomobila;
 import enumeracija.Pol;
+import enumeracija.Proizvodjac;
+import enumeracija.VrstaVozila;
 import osobe.Dispecer;
 import osobe.Musterija;
+import osobe.Vozac;
 import radSaFajlovima.RadSaDatotekama;
+import taxiSluzba.Automobil;
 
 public class KolekcijeTest {
 
 	public static void main(String[] args) {
 		RadSaDatotekama rsd = new RadSaDatotekama();
+		ArrayList<Automobil> aa = new ArrayList<Automobil>();
+
+		Automobil automobil = new Automobil(1, ModelAutomobila.GOLF4, Proizvodjac.VOLKSWAGEN, "2004", "12-12", VrstaVozila.PUTNICKI_AUTOMOBIL);
+		aa.add(automobil);
+		rsd.upisiAutomobile(aa);
+		
+		ArrayList<Musterija> musterije = new ArrayList<Musterija>();
 		System.out.println("Dodavanje musterije: ");
 		Musterija testMusterija = new Musterija(2, "Mare", "mare123", "Marko", "Markovic", "15210521152", "Alekse Santica 5", Pol.MUSKI, "065424141");
-		
+		Musterija testMusterija2 = new Musterija(3, "Mare", "ranko123", "Ranko", "Markovic", "15210521152", "Alekse Santica 5", Pol.MUSKI, "065424141");
+		musterije.add(testMusterija);
+		musterije.add(testMusterija2);
+		rsd.upisiMusterije(musterije);
 		System.out.println(testMusterija);
 		
 		ArrayList<Dispecer> ucitaniDispeceri = RadSaDatotekama.ucitajDispecere();
@@ -33,6 +48,25 @@ public class KolekcijeTest {
 			System.out.println(musterija);
 			
 		}
+		
+		ArrayList<Automobil> ucitaniAutomobili = RadSaDatotekama.ucitajAutomobile();
+		
+		System.out.println("Ucitani automobili");
+		for (Automobil automobil2 : ucitaniAutomobili) {
+			
+			System.out.println(automobil2);
+		} 
+			
+			
+			
+		
+		ArrayList<Vozac> ucitaniVozaci = RadSaDatotekama.ucitajVozace();
+		System.out.println("Ucitani vozaci");
+		for (Vozac vozac : ucitaniVozaci) {
+			System.out.println(vozac);
+		
+		}
+		
 	}
 	
 	
--- TaxiSluzba/src/radSaFajlovima/RadSaDatotekama.java
+++ TaxiSluzba/src/radSaFajlovima/RadSaDatotekama.java
@@ -1,5 +1,6 @@
 package radSaFajlovima;
 
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -8,12 +9,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import enumeracija.ModelAutomobila;
 import enumeracija.Pol;
+import enumeracija.Proizvodjac;
 import enumeracija.TelOdeljenja;
+import enumeracija.VrstaVozila;
 import osobe.Dispecer;
 import osobe.Musterija;
+import osobe.Vozac;
+import taxiSluzba.Automobil;
 
 public class RadSaDatotekama {
+	
+	private static ArrayList<Automobil> automobil = new ArrayList<Automobil>();
 	
 	public static ArrayList<Dispecer> ucitajDispecere() {
 		 ArrayList<Dispecer> dispeceri = new ArrayList<Dispecer>();
@@ -80,8 +88,112 @@ public class RadSaDatotekama {
 				return musterije;
 	}
 	
-	public ArrayList<Musterija> upisiMusterije() {
-			ArrayList<Musterija> musterije = new ArrayList<Musterija>();
+	public static ArrayList<Vozac> ucitajVozace() {
+		ArrayList<Vozac> vozaci = new ArrayList<Vozac>();
+				
+				try {
+					
+					File file = new File("src/txt/vozaci.txt");
+					BufferedReader reader = new BufferedReader(new FileReader(file));
+					String line;
+					while((line = reader.readLine()) !=null) {
+						String[] lineSplit = line.split("\\|");
+						int id = Integer.parseInt(lineSplit[0]);
+						String korIme = lineSplit[1];
+						String lozinka = lineSplit[2];
+						String ime = lineSplit[3];
+						String prezime = lineSplit[4];
+						String jMBG = lineSplit[5];
+						String adresa = lineSplit[6];
+						Pol pol = Pol.values()[Integer.parseInt(lineSplit[7])];
+						String brTel = lineSplit[8];
+						int plata = Integer.parseInt(lineSplit[9]);
+						String brojClanskeKarte = lineSplit[10];
+						int idd = Integer.parseInt(lineSplit[11]);
+						Automobil automobil = nadjiAutomobil(idd);
+						Boolean obrisan = Boolean.parseBoolean(lineSplit[12]);
+						Vozac vozac = new Vozac(id, korIme, lozinka, ime, prezime, jMBG, adresa, pol, brTel, plata, brojClanskeKarte, automobil, obrisan);
+						vozaci.add(vozac);
+					}
+					reader.close();
+					
+				} catch (IOException e) {
+					System.out.println("Greska prilikom citanja musterija");
+				}
+				
+				return vozaci;
+	}
+	
+	public static Automobil nadjiAutomobil(int id) {
+        for(Automobil automobil: automobil) {
+            if(automobil.getId() == id) {
+                return automobil;
+            }
+        }
+        return null;
+    }
+	
+	public static ArrayList<Automobil> ucitajAutomobile() {
+		
+				
+				try {
+					
+					File file = new File("src/txt/automobili.txt");
+					BufferedReader reader = new BufferedReader(new FileReader(file));
+					String line;
+					while((line = reader.readLine()) !=null) {
+						String[] lineSplit = line.split("\\|");
+						int id = Integer.parseInt(lineSplit[0]);
+						String modelString = lineSplit[1];
+						ModelAutomobila model = ModelAutomobila.valueOf(modelString);
+						
+						String proizvodjacString = lineSplit[2];
+						Proizvodjac proizvodjac = Proizvodjac.valueOf(proizvodjacString);
+						String godinaProizvodnje = lineSplit[3];
+						String brRegOznake = lineSplit[4];
+						String vrstaVozilaString = lineSplit[5];
+						VrstaVozila vrsta = VrstaVozila.valueOf(vrstaVozilaString);
+						Automobil a = new Automobil(id, model, proizvodjac, godinaProizvodnje, brRegOznake, vrsta);
+						automobil.add(a);
+				
+					
+					}
+					reader.close();
+					
+				} catch (IOException e) {
+					System.out.println("Greska prilikom citanja musterija");
+				}
+				
+				return automobil;
+	}
+	public ArrayList<Automobil> upisiAutomobile(ArrayList<Automobil> automobili) {
+		
+		
+		File file = new File("src/txt/automobili.txt");
+		
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			String sadrzaj = "";
+			for (Automobil automobil : automobili) {
+				sadrzaj += automobil.getId() + "|" + automobil.getModel() + "|" +automobil.getProizvodjac() + "|" + automobil.getGodinaProizvodnje()
+				+"|" + automobil.getBrRegOznake() + "|" + automobil.getVrstaVozila() + "\n";
+			} 
+			
+			writer.write(sadrzaj);
+			writer.close();
+		} catch (Exception e) {
+			System.out.println("Greska prilikom upisa automobila u fajl.");
+		}
+		
+		return automobil;
+	}
+						
+						
+	
+	
+	
+	public ArrayList<Musterija> upisiMusterije(ArrayList<Musterija> musterije) {
+			
 			
 			File file = new File("src/txt/musterije.txt");
 			
@@ -92,7 +204,7 @@ public class RadSaDatotekama {
 					sadrzaj += musterija.getId() + "|" + musterija.getKorIme() + "|" + musterija.getLozinka() + "|" 
 							+ musterija.getIme() + "|" + musterija.getPrezime() + "|"
 							+ musterija.getjMBG() + "|" + musterija.getAdresa() + "|" 
-							+ musterija.getPol().ordinal() + "|" + musterija.getBrTel() + "\n";
+							+ musterija.getPol().ordinal() + "|" + musterija.getBrTel() + "\n";	
 				}
 				
 				writer.write(sadrzaj);
@@ -100,9 +212,11 @@ public class RadSaDatotekama {
 			} catch (Exception e) {
 				System.out.println("Greska prilikom upisa musterija u fajl.");
 			}
+			
 			return musterije;
 		}
-		
+	
+	
 	
 	
 
